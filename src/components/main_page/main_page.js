6f5dcce433d8781ed40672c55400f8c96b4dc5be
Adrian Ren <adrian.ren@outlook.com>
2018-02-10 13:43:01 +1300

    app has been divided into several components

---
+++ src/components/main_page/main_page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import CurrentlyReading from './currently_reading';
+import WantToRead from './want_read';
+import Read from './read';
+
+const MainPage = (props) => {
+  return(
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <CurrentlyReading />
+          {/*Want to Read*/}
+          <WantToRead />
+          {/*Read*/}
+          <Read />
+        </div>
+      </div>
+      <div className="open-search">
+        <a onClick={props.onAddClick}>Add a book</a>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage;
